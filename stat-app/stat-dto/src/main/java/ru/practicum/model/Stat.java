8a3b469d03278d13ad00df086fbf8cfade1e8ba8
mybogdan <bogdan.vaseev@gmail.com>
2024-02-02 19:49:38 +0300

    v1.0

--- stat-app/stat-dto/src/main/java/ru/practicum/model/Stat.java
+++ stat-app/stat-dto/src/main/java/ru/practicum/model/Stat.java
@@ -11,7 +11,7 @@ import java.time.LocalDateTime;
 public class Stat {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long Id;
+    private Long id;
     @Column
     private String app;
     @Column
